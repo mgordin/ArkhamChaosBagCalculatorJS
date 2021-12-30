b0bc6883901d2f534c9c7cb48e2e2582b062c2c3
Mikhail Gordin <mikhail.gordin@gmail.com>
2021-12-30 00:51:15 -0800

    Vue stuff works at a very basic level

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 // Params
-var autofail_value = -999;
+
 var skull_value = -2;
 var cultist_value = -2;
 var tablet_value = -3;
 var squiggle_value = -4;
+var autofail_value = -999;
 var variable_tokens = ['Skull', 'Cultist', 'Tablet', 'Squiggle']
+
+var data = {
+    cultist_value: -2,
+    skull_value: -2,
+    tablet_value: -3,
+    squiggle_value: -4,
+    autofail_value: -999,
+    message: 'Hello Vue.js!'
+}
 
 // Functions
 function range(start, end) {
@@ -12,15 +22,16 @@ function range(start, end) {
     return [start, ...range(start + 1, end)];
 }
 
-function calculationStep(remainingOptions, previousTotal, probMod, pastFrost, allResults) {
+function calculationStep(remainingOptions, previousTotal, probMod, pastFrost, drawCount, allResults) {
     remainingOptions.forEach(function (token, i) {
         var total = previousTotal + token[0];
-        if (probMod < 0.000001) {
+        //if (probMod < 0.000001) {
+        if (drawCount > 4) {
             allResults.push([autofail_value, probMod])
         } else if (token[1]) {
             if (!(pastFrost && token[2] == 'Frost')) {
                 calculationStep(
-                    remainingOptions.slice(0, i).concat(remainingOptions.slice(i + 1)), total, probMod / (remainingOptions.length - 1), token[2] == 'Frost', allResults)
+                    remainingOptions.slice(0, i).concat(remainingOptions.slice(i + 1)), total, probMod / (remainingOptions.length - 1), token[2] == 'Frost', drawCount + 1, allResults)
             } else {
                 allResults.push([autofail_value, probMod])
             }
@@ -86,23 +97,68 @@ function sumStuffDown(prob, target) {
 
 // Test it out
 
-var options = [[1, false, 'Star']].concat(
-    [[1, false, '+1']],
-    [[0, false, '0'], [0, false, '0']],
-    [[-1, false, '-1'], [-1, false, '-1'], [-1, false, '-1']],
-    [[-2, false, '-2'], [-2, false, '-2']],
-    [[cultist_value, false, 'Cultist'], [cultist_value, false, 'Cultist']],
-    [[-3, false, '-3']],
-    [[tablet_value, false, 'Tablet']],
-    [[-4, false, '-4']],
-    [[skull_value, false, 'Skull'], [skull_value, false, 'Skull']],
-    [[squiggle_value, false, 'Squiggle'], [squiggle_value, false, 'Squiggle']],
-    [[-1, true, 'Frost'], [-1, true, 'Frost'], [-1, true, 'Frost']],
-    [[autofail_value, false, 'Autofail']]
-);
+function run(options) {
+    var allResults = []
+    calculationStep(options, 0, 1 / options.length, false, 1, allResults)
+    cumulative = aggregate(allResults)
+    return cumulative
+}
 
-var allResults = []
-calculationStep(options, 0, 1 / options.length, false, allResults)
-cumulative = aggregate(allResults)
+function testRun(cultist_value, tablet_value, skull_value, squiggle_value, autofail_value) {
+    console.log("Cultist value is ", cultist_value)
+    var options = [[1, false, 'Star']].concat(
+        [[1, false, '+1']],
+        [[0, false, '0'], [0, false, '0']],
+        [[-1, false, '-1'], [-1, false, '-1'], [-1, false, '-1']],
+        [[-2, false, '-2'], [-2, false, '-2']],
+        [[cultist_value, false, 'Cultist'], [cultist_value, false, 'Cultist']],
+        [[-3, false, '-3']],
+        [[tablet_value, false, 'Tablet']],
+        [[-4, false, '-4']],
+        [[skull_value, false, 'Skull'], [skull_value, false, 'Skull']],
+        [[squiggle_value, false, 'Squiggle'], [squiggle_value, false, 'Squiggle']],
+        [[-1, true, 'Frost'], [-1, true, 'Frost'], [-1, true, 'Frost']],
+        [[autofail_value, false, 'Autofail']]
+    );
+
+    var allResults = []
+    calculationStep(options, 0, 1 / options.length, false, 1, allResults)
+    cumulative = aggregate(allResults)
+    return cumulative
+}
+
+function probabilityPlot(p) {
+    xValue = range(-2, 5);
+    yValue = [
+        Math.round(p[-2]),
+        Math.round(p[-1]),
+        Math.round(p[0]),
+        Math.round(p[1]),
+        Math.round(p[2]),
+        Math.round(p[3]),
+        Math.round(p[4]),
+        Math.round(p[5])
+    ];
+    var data = [{
+        x: xValue,
+        y: yValue,
+        type: 'bar',
+        text: yValue.map(String),
+        textposition: 'auto'
+    }];
+
+    return Plotly.newPlot('myDiv', data);
+}
 
 // Vue stuff
+
+var app10 = new Vue({
+    el: '#app-10',
+    data: data,
+    methods: {
+        getProbabilitiesMessage: function () {
+            this.message = JSON.stringify(testRun(this.cultist_value, this.tablet_value, this.skull_value, this.squiggle_value, this.autofail_value))
+            probabilityPlot(testRun(this.cultist_value, this.tablet_value, this.skull_value, this.squiggle_value, this.autofail_value))
+        }
+    }
+})
