18b9c457cc7d3b7fa3c7ddee195bcd4fec90e8de
Mikhail Gordin <mikhail.gordin@gmail.com>
2021-12-31 21:50:09 -0800

    Saves state between sessions!

--- app.js
+++ app.js
@@ -1,76 +1,12 @@
-// Params
-var data = {
-    tokens: {
-        '+1': [1, 1, false, null],
-        '0': [2, 0, false, null],
-        '-1': [3, -1, false, null],
-        '-2': [2, -2, false, null],
-        '-3': [1, -3, false, null],
-        '-4': [1, -4, false, null],
-        '-5': [0, -5, false, null],
-        'skull': [2, -2, false, null],
-        'cultist': [2, -2, false, null],
-        'tablet': [1, -3, false, null],
-        'squiggle': [2, -4, false, null],
-        'star': [1, 1, false, null],
-        'autofail': [1, -999, false, null],
-        'bless': [0, 2, true, null],
-        'curse': [0, -2, true, null],
-        'frost': [4, -1, true, 'frost']
-    },
-    modifiers: {
-        '+1': [],
-        '0': [],
-        '-1': [],
-        '-2': [],
-        '-3': [],
-        '-4': [],
-        '-5': [],
-        'skull': [],
-        'cultist': [],
-        'tablet': [],
-        'squiggle': [],
-        'star': [],
-        'autofail': [],
-        'bless': [],
-        'curse': [],
-        'frost': []
-    },
-    redraw_max: 4,
-    variable_tokens: ['skull', 'cultist', 'tablet', 'squiggle'],
-    tokenOptions: [
-        { text: "Bless", value: "bless" },
-        { text: "Curse", value: "curse" },
-        { text: "Cultist", value: "cultist" },
-        { text: "Frost", value: "frost" },
-        { text: "Tablet", value: "tablet" },
-        { text: "Skull", value: "skull" },
-        { text: "Squiggle", value: "squiggle" }
+//  ---------------- Function definitions ------------------
 
-    ],
-    abilitiesActive: [],
-    abilityOptions: [
-        { text: 'Jim Culver', value: 'JimCulver' },
-        { text: 'Ritual Candles', value: 'RitualCandles1' },
-        { text: 'Ritual Candles', value: 'RitualCandles2' }
-    ],
-    abilityEffects: {
-        'JimCulver': {
-            'skull': ['s', 0]
-        },
-        'RitualCandles1': {
-            'skull': ['a', 1],
-            'cultist': ['a', 1],
-            'tablet': ['a', 1],
-            'squiggle': ['a', 1]
-        },
-        'RitualCandles2': {
-            'skull': ['a', 2],
-            'cultist': ['a', 2],
-            'tablet': ['a', 2],
-            'squiggle': ['a', 2]
-        }
-    }
+function saveData(name, data) {
+    localStorage.setItem(name, JSON.stringify(data));
+}
+
+function loadData(name) {
+    data = JSON.parse(localStorage.getItem(name));
+    return data
 }
 
 // Bag is a list of lists, each interior list is one token as 
@@ -93,7 +29,7 @@ function prepareModifiers(abilitiesActive, abilityEffects, modifiers) {
         abilitiesActive.forEach(function(ability, i) {
             var abilityEffect = abilityEffects[ability];
             for (const [k, v] of Object.entries(abilityEffect)) {
-                if (modifiers[k].length == 0) {
+                if (modifiers[k].length == 0 || abilityEffect[k][0] == 's') {
                     modifiers[k] = v
                 } else if (modifiers[k][0] == 'a') {
                     modifiers[k][1] += abilityEffect[k][1]
@@ -124,9 +60,11 @@ function calculateTotal(previousTotal, token, modifiers) {
 function calculationStep(remainingOptions, previousTotal, probMod, lastDraw, drawCount, autofail_value, redraw_max, allResults, modifiers) {
     remainingOptions.forEach(function(token, i) {
         // Calculate result, assuming now additional stuff happening
+        console.log('last: ', lastDraw, 'current: ', token[2], 'autofail after: ', token[3])
         if (lastDraw && lastDraw == token[3]) { // If the previous draw would make this an autofail, do that
+            console.log('autofailed on ', lastDraw, token[2], token[3])
             allResults.push([autofail_value, probMod]);
-        } else if (token[1]) { // If this is a token that prompts a redraw, do that
+        } else if (token[1] && modifiers[token[2]][2] != 'noRedraw') { // If this is a token that prompts a redraw, do that
             if (drawCount + 1 > redraw_max) { // If this draw is too many redraws - treat as an autofail to speed up calculation
                 allResults.push([autofail_value, probMod]);
             } else {
@@ -202,6 +140,8 @@ function run(tokens, abilitiesActive, abilityEffects, modifiers, redraw_max) {
     prepareModifiers(abilitiesActive, abilityEffects, modifiers)
     calculationStep(bag, 0, 1 / bag.length, null, 1, tokens['autofail'][1], redraw_max, allResults, modifiers)
     cumulative = aggregate(allResults)
+    saveData(saveName, data)
+
     return cumulative
 }
 
@@ -249,14 +189,107 @@ function probabilityPlot(p) {
             },
             tickfont: {
                 size: 16
-            }
+            },
+            range: [0, 101]
         }
     }
 
     return Plotly.newPlot('probPlot', data, layout);
 }
 
-// Vue stuff
+
+//  ---------------- Set up the page and params ------------------
+
+// Params
+var saveName = "mgArkhamChaosBagData"
+var data = {
+    tokens: {
+        '+1': [1, 1, false, null],
+        '0': [2, 0, false, null],
+        '-1': [3, -1, false, null],
+        '-2': [2, -2, false, null],
+        '-3': [1, -3, false, null],
+        '-4': [1, -4, false, null],
+        '-5': [0, -5, false, null],
+        'skull': [2, -2, false, null],
+        'cultist': [2, -2, false, null],
+        'tablet': [1, -3, false, null],
+        'elderThing': [2, -4, false, null],
+        'star': [1, 1, false, null],
+        'autofail': [1, -999, false, null],
+        'bless': [0, 2, true, null],
+        'curse': [0, -2, true, null],
+        'frost': [4, -1, true, 'frost']
+    },
+    modifiers: {
+        '+1': [],
+        '0': [],
+        '-1': [],
+        '-2': [],
+        '-3': [],
+        '-4': [],
+        '-5': [],
+        'skull': [],
+        'cultist': [],
+        'tablet': [],
+        'elderThing': [],
+        'star': [],
+        'autofail': [],
+        'bless': [],
+        'curse': [],
+        'frost': []
+    },
+    redraw_max: 4,
+    variable_tokens: ['skull', 'cultist', 'tablet', 'elderThing'],
+    tokenOptions: [
+        { text: "", value: null },
+        { text: "Bless", value: "bless" },
+        { text: "Curse", value: "curse" },
+        { text: "Cultist", value: "cultist" },
+        { text: "Elder Thing", value: "elderThing" },
+        { text: "Frost", value: "frost" },
+        { text: "Tablet", value: "tablet" },
+        { text: "Skull", value: "skull" }
+
+    ],
+    abilitiesActive: [],
+    abilityOptions: [
+        { text: 'Defiance (2)', value: 'Defiance2XP' },
+        { text: 'Jim Culver', value: 'JimCulver' },
+        { text: 'Ritual Candles', value: 'RitualCandles1' },
+        { text: 'Ritual Candles', value: 'RitualCandles2' }
+    ],
+    abilityEffects: {
+        'Defiance2XP': {
+            'skull': ['s', 0, 'noRedraw'],
+            'cultist': ['s', 0, 'noRedraw'],
+            'tablet': ['s', 0, 'noRedraw'],
+            'elderThing': ['s', 0, 'noRedraw']
+        },
+        'JimCulver': {
+            'skull': ['s', 0]
+        },
+        'RitualCandles1': {
+            'skull': ['a', 1],
+            'cultist': ['a', 1],
+            'tablet': ['a', 1],
+            'elderThing': ['a', 1]
+        },
+        'RitualCandles2': {
+            'skull': ['a', 2],
+            'cultist': ['a', 2],
+            'tablet': ['a', 2],
+            'elderThing': ['a', 2]
+        }
+    }
+}
+
+let tryData = loadData(saveName)
+if (tryData) {
+    data = tryData
+}
+
+probabilityPlot(run(data.tokens, data.abilitiesActive, data.abilityEffects, data.modifiers, data.redraw_max))
 
 var app10 = new Vue({
     el: '#app-10',
